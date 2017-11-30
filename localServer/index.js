1adecf3820287369f85f07b5cf13dd647503f5a6
Carl Benson <bensocar@oregonstate.edu>
2017-11-30 14:10:12 -0800

    Removed information

--- localServer/index.js
+++ localServer/index.js
@@ -1,8 +1,8 @@
 var socket = require("socket.io-client")("http://34.215.212.179:3001");
 var MongoClient = require('mongodb').MongoClient, f = require('util').format, assert = require('assert');
 
-var user = encodeURIComponent('node');
-var password = encodeURIComponent('capstone');
+var user = "";
+var password = "";
 var authMechanism = 'DEFAULT';
 var url = f('mongodb://%s:%s@34.215.212.179:27017/sales?authMechanism=%s',
   user, password, authMechanism);
