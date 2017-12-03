efc154a3ee73c9577d2fdac4a9e9f2929b512dd5
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-12-03 12:50:02 -0800

    Changes alexa intents and clean up of console logs

--- AWS-Server/index.js
+++ AWS-Server/index.js
@@ -3,20 +3,40 @@ var webSocketServer = require('http').createServer(app);
 var io = require('socket.io')(webSocketServer);
 
 io.on('connection', function (socket) {
-   socket.broadcast.emit("connected", {
-      message: "Ping!"
-   });
-   console.log("ping!")
+    socket.emit("connected", {
+        message: "Ping!"
+    });
+    console.log("ping!");
+    socket.on('message', function (msg) {
+        console.log('in');
+        socket.emit('message', 'its working');
+        socket.emit('message', 'second');
+    });
 });
 
 app.get('/', (req, res) => {
-   res.send("Hello world!");
-})
+    res.send("Hello world!");
+});
+
+
+
+app.get('/:which/:type/:time/', (req, res) => {
+    res.json({
+        data: 'here is ' + req.params.which + ' ' + req.params.type + ' for ' + req.params.time
+    });
+});
 
 app.get("/test", (req, res) => {
-   console.log("You did the thing!!!");
-   res.send("You did the thing!!!")
-})
+    console.log("You did the thing!!!");
+    res.json({ data: "You did the thing!!!" });
+});
 
-app.listen(3005, () => console.log("Server up and running on port 3005!"))
-webSocketServer.listen(3000, () => console.log("Websocket running on port 3000!"));
+app.get('/repeat', (req, res) => {
+    console.log(req)
+    res.json({ data: "You did the thing!!!" });
+});
+
+
+app.get('/ryan', (req, res) => {
+    res.json({ data: "Ryan smells" });
+});
