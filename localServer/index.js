46d43d0b981cf896a0c012805479d98f56c21dbc
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-12-18 21:00:46 -0800

    Indentation and randomization within the data generator

--- localServer/index.js
+++ localServer/index.js
@@ -1,5 +1,6 @@
-var socket = require("socket.io-client")("http://34.215.212.179:3000")
-
-socket.on('connect', function () {
-   console.log("connected!!")
+var socket = require('socket.io-client')('http://34.215.212.179:3000');
+socket.on('connect', function(msg){
+    console.log(msg)
 });
+socket.on('event', function(data){});
+socket.on('disconnect', function(){});
