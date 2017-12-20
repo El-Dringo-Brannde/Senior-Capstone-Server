6206ec5ef170d6a3ccd82b712e30faf72d647129
Carl Benson <bensocar@oregonstate.edu>
2017-12-19 18:47:08 -0800

    Receive DB results from AWS

--- localServer/index.js
+++ localServer/index.js
@@ -1,27 +1,9 @@
-var socket = require("socket.io-client")("http://34.215.212.179:3001");
-var MongoClient = require('mongodb').MongoClient, f = require('util').format, assert = require('assert');
-
-var user = "";
-var password = "";
-var authMechanism = 'DEFAULT';
-var url = f('mongodb://%s:%s@34.215.212.179:27017/sales?authMechanism=%s',
-  user, password, authMechanism);
-
-MongoClient.connect(url, function(err, db){
-  assert.equal(null, err);
-  console.log("connected");
-
-  db.close();
-})
-
-socket.on('connect', function () {
-  console.log("connected")
-
-  socket.on('query', function(msg){
-    console.log(msg);
-
-    MongoClient.connect(url, function(err, db){
-
-    })
-  });
+var socket = require('socket.io-client')('http://34.215.212.179:3002');
+socket.on('connect', function(msg){
+    console.log(msg)
 });
+socket.on('data', function(data){
+	console.log(data);
+});
+socket.on('event', function(data){});
+socket.on('disconnect', function(){});
