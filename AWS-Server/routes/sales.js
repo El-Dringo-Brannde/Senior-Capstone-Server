f08dd917ab3ee01d09799e436595ce2f55b463f3
NipunBathini <nbathini@live.com>
2018-01-15 19:23:44 -0800

    Added Express Validation

--- AWS-Server/routes/sales.js
+++ AWS-Server/routes/sales.js
@@ -1,5 +1,9 @@
 var router = require('express').Router();
+var expressValidator = require('express-validator');
 var sales = require('./../logic/sales');
+let util = require('util');
+
+
 
 // All routes here are prefixed by the /sales route
 module.exports = function (mongo, socket) {
@@ -44,6 +48,27 @@ module.exports = function (mongo, socket) {
          data: data
       })
    });
+
+   // Test for Express-validator
+    router.get('/test_validator', (req, response, next) => {
+       req.checkQuery('state', '"State" can not be empty and must be a string').isNotEmpty();
+       req.checkQuery('city', '"City" not be empty').isNotEmpty();
+       req.getValidationResult().then((validationResult) => {
+          if(!validationResult.isEmpty()) {
+              response.json({
+                  result: "failed",
+                  message: `Validation errors: ${util.inspect(validationResult.array())}`
+              });
+              return;
+          }
+          //otherwise show
+            response.json({
+                result: "ok",
+                messsage: `Validate input successfully. Input params = ${util.inspect(request.query)}`
+            });
+          });
+    });
+
 
    return router;
 };
