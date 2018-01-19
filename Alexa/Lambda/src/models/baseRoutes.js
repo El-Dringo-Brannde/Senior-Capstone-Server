9ac1af61f9a1ba3b4f653809a5711a5a7b429cf7
Unknown <bensocar@oregonstate.edu>
2018-01-18 22:06:46 -0800

    Updated IP, Removed unused var, Reduced complexity while testing

--- Alexa/Lambda/src/models/baseRoutes.js
+++ Alexa/Lambda/src/models/baseRoutes.js
@@ -1,6 +1,6 @@
 module.exports = class baseRoutes {
    constructor() {
-      this.serverURL = 'http://35.169.224.183:3105/';
+      this.serverURL = 'http://34.215.212.179:3105/';
       this.sessionAttributes = {};
 
       this.buildResponse = require('./../buildResponse');
@@ -14,28 +14,31 @@ module.exports = class baseRoutes {
    }
 
    buildQueryString(intentSlots) {
+      var route = '';
       var query = '';
       console.log(intentSlots)
 
-      if (intentSlots.Type && intentSlots.Value) {
-         query = intentSlots.Type.value + '/'
+      if (intentSlots.Type && intentSlots.Type.value) {
+         route = intentSlots.Type.value + '/'
          delete intentSlots.Type
       }
-      if (intentSlots.Selector) {
+      if (intentSlots.Selector && intentSlots.Selector.value) {
          query += intentSlots.Selector.value;
          delete intentSlots.Selector
       }
 
       query += '?'
       for (var i in intentSlots)
-         if (intentSlots[i].value)
-            query += intentSlots[i].name + '=' + intentSlots[i].value.toLowerCase() + '&'
-      console.log(query)
-      return query
+         if (intentSlots[i].value && intentSlots[i].name){
+           route += intentSlots[i].name + '/'
+           query += intentSlots[i].name + '=' + intentSlots[i].value.toLowerCase() + '&'
+          }
+      console.log(route.slice(0, -1) + query.slice(0, -1));
+      return route.slice(0, -1) + query;
    }
 
    sendBackReturnedData(intentName, data, callback) {
-      let speechOutput = "Ok, I went to the route. And I got " + JSON.parse(data).data;
+      let speechOutput = "Ok, I went to the route. And I got ";// + JSON.parse(data).data;
       let repromptText = "Ok, I'm trying again.";
       callback(this.sessionAttributes, this.buildResponse(intentName, speechOutput, repromptText, false));
    }
@@ -43,13 +46,13 @@ module.exports = class baseRoutes {
 
    parseRoute(intent, sessionID, callback) {
       const intentName = intent.name;
-      const Route = intent.slots.Route;
       const sessionQuery = 'session=' + sessionID.toLowerCase();
 
       if (Object.keys(intent.slots).length == 1)
          this.sendRequest(routeVal.toLowerCase(), sessionQuery, intentName, callback)
       else {
          let route = this.buildQueryString(intent.slots)
+         console.log("Done with intent");
          this.sendRequest(route, sessionQuery, intentName, callback)
       }
    }
