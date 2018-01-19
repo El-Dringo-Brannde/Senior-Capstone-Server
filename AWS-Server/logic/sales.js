4cd2a6cc15ba0beaba19bb8b4d74d9bbb0070601
Unknown <bensocar@oregonstate.edu>
2018-01-18 21:25:17 -0800

    Added missing function

--- AWS-Server/logic/sales.js
+++ AWS-Server/logic/sales.js
@@ -22,6 +22,25 @@ module.exports = class sales extends salesAgg {
       return await this.aggregate(agg)
    }
 
+   async allByCityState(city, state) {
+      let matchObj = { city: city, state: state }
+      let returnObj = {};
+
+      let brands = await this.read(matchObj);
+      brands = brands[0].brands;
+
+      let agg1 = this.aggregateBuilder.matchProjectAgg(matchObj);
+      returnObj.totalSales = await this.aggregate(agg1);
+      returnObj.totalSales = returnObj.totalSales[0]
+
+      for (let brand of brands) {
+         let agg = this.aggregateBuilder.cityStateByBrand(city, state, brand);
+         returnObj[brand] = await this.aggregate(agg)
+         returnObj[brand] = returnObj[brand][0]
+      }
+      return returnObj
+   } // ghetto way of doing math NOT in mongoDB
+
    async cityStateBrand(city, state) {
       let matchObj = { city: city, state: state }
       let returnObj = {};
