f34efaf0e583ae19ed75f35380b12a3f76ba3811
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2018-01-28 13:09:53 -0800

    Fixed query parameters

--- AWS-Server/paramValidation/sales.js
+++ AWS-Server/paramValidation/sales.js
@@ -1,148 +1,65 @@
 var router = require('express').Router();
-var expressValidator = require('express-validator');
+var { check, validationResult } = require('express-validator/check');
 let util = require('util');
 
-class paramVal {
-    constructor() {
-    }
-    // Test for Express-validator
+class salesValidator {
+   constructor() { }
 
-    //Param Validation for state
-    validateState(state) {
-        router.get('/test_validator', (req, response, next) => {
-            req.checkQuery('state', '"State" can not be empty and must be a string').notEmpty().isString();
-            req.getValidationResult().then((validationResult) => {
-                if (!validationResult.isEmpty()) {
-                    response.json({
-                        result: "failed",
-                        message: `Validation errors: ${util.inspect(validationResult.array())}`
-                    });
-                    return;
-                }
-                //otherwise show
-                response.json({
-                    result: "ok",
-                    message: `Validate input successfully. Input params = ${util.inspect(request.query)}`
-                });
-            });
-        });
-    }
+   checkResult(req, res) {
+      let errors = validationResult(req)
+      if (!errors.isEmpty())
+         res.json({
+            error: errors.mapped()
+         })
+   }
 
-    //Param Validation for city
-    validateCity(city) {
-        router.get('/test_validator', (req, response, next) => {
-            req.checkQuery('city', '"City" not be empty').notEmpty().isString();
-            req.getValidationResult().then((validationResult) => {
-                if (!validationResult.isEmpty()) {
-                    response.json({
-                        result: "failed",
-                        message: `Validation errors: ${util.inspect(validationResult.array())}`
-                    });
-                    return;
-                }
-                //otherwise show
-                response.json({
-                    result: "ok",
-                    message: `Validate input successfully. Input params = ${util.inspect(request.query)}`
-                });
-            });
-        });
-    }
+   //Param Validation for state
+   state() {
+      return [
+         check('state')
+            .exists()
+            .isAlpha(),
+         check('group')
+            .isIn(['color', 'name', 'age', 'model', 'brand', 'price', 'date', 'license'])
+            .exists(),
+         check('userID')
+            .isAscii()
+            .exists()
+      ] // check against US state array
+   }
 
+   //Param Validation for city
+   city() {
+      return [
+         check('city')
+            .exists()
+            .isAlpha(),
+         check('group')
+            .isIn(['color', 'name', 'age', 'model', 'brand', 'price', 'date', 'license'])
+            .exists(),
+         check('userID')
+            .isAscii()
+            .exists()
+      ] // check against US state array
+   }
 
-    //Param validation for sales
-    validateSales(price, date, dealer_id, name, license, color, age, brand) {
-        router.get('/test_validator', (req, response, next) => {
-            req.checkQuery('price', '"price" can not be empty and must be a int').notEmpty().isInt();
-            req.checkQuery('date', '"date" not be empty Example: 2017-09-30 17:00:00.000').notEmpty().isDate();
-            req.checkQuery('dealer_id', '"dealer id" can not be empty and must be a int').notEmpty().isInt();
-            req.checkQuery('name', '"name" can not be empty must be a string').notEmpty().isString();
-            req.checkQuery('license', '"license" can not be empty must be a string').notEmpty().isString();
-            req.checkQuery('color', '"color" can not be empty must be a string').notEmpty().isString();
-            req.checkQuery('age', '"age" can not be empty and must be a int').notEmpty().isInt();
-            req.checkQuery('brand', '"brand" can not be empty must be a string').notEmpty().isString();
-            req.getValidationResult().then((validationResult) => {
-                if (!validationResult.isEmpty()) {
-                    response.json({
-                        result: "failed",
-                        message: `Validation errors: ${util.inspect(validationResult.array())}`
-                    });
-                    return;
-                }
-                //otherwise show
-                response.json({
-                    result: "ok",
-                    messsage: `Validate input successfully. Input params = ${util.inspect(request.query)}`
-                });
-            });
-        });
-    }
-
-    //Param validation for Year to Date
-    validateYTD(ytd) {
-        router.get('/test_validator', (req, response, next) => {
-            req.checkQuery('ytd', '"Year to Date" can not be empty must be Integer').notEmpty().isInt();
-            req.getValidationResult().then((validationResult) => {
-                if (!validationResult.isEmpty()) {
-                    response.json({
-                        result: "failed",
-                        message: `Validation errors: ${util.inspect(validationResult.array())}`
-                    });
-                    return;
-                }
-                //otherwise show
-                response.json({
-                    result: "ok",
-                    message: `Validate input successfully. Input params = ${util.inspect(request.query)}`
-                });
-            });
-        });
-    }
-
-    //Param validation for Month to Date
-    validateMTD(mtd) {
-        router.get('/test_validator', (req, response, next) => {
-            req.checkQuery('mtd', '"Month to Date" can not be empty must be Integer').notEmpty().isInt();
-            req.getValidationResult().then((validationResult) => {
-                if (!validationResult.isEmpty()) {
-                    response.json({
-                        result: "failed",
-                        message: `Validation errors: ${util.inspect(validationResult.array())}`
-                    });
-                    return;
-                }
-                //otherwise show
-                response.json({
-                    result: "ok",
-                    message: `Validate input successfully. Input params = ${util.inspect(request.query)}`
-                });
-            });
-        });
-    }
-
-
-    //Param validation for id
-    validateID(id) {
-        router.get('/test_validator', (req, response, next) => {
-            req.checkQuery('id', '"id" can not be empty must be Integer').notEmpty().isInt();
-            req.getValidationResult().then((validationResult) => {
-                if (!validationResult.isEmpty()) {
-                    response.json({
-                        result: "failed",
-                        message: `Validation errors: ${util.inspect(validationResult.array())}`
-                    });
-                    return;
-                }
-                //otherwise show
-                response.json({
-                    result: "ok",
-                    message: `Validate input successfully. Input params = ${util.inspect(request.query)}`
-                });
-            });
-        });
-    }
-
-
+   //Param validation for city & state
+   cityState() {
+      return [
+         check('city')
+            .exists()
+            .isAlpha(),
+         check('state')
+            .exists()
+            .isAlpha(),
+         check('group')
+            .isIn(['color', 'name', 'age', 'model', 'brand', 'price', 'date', 'license'])
+            .exists(),
+         check('userID')
+            .isAscii()
+            .exists()
+      ] // check against US state array
+   }
 }
 
-module.exports = new paramVal();
+module.exports = new salesValidator();
--- AWS-Server/routes/sales.js
+++ AWS-Server/routes/sales.js
@@ -1,56 +1,62 @@
 var router = require('express').Router();
-var expressValidator = require('express-validator');
 var sales = require('./../logic/sales');
 let util = require('util');
-
+var { check, validationResult } = require('express-validator/check');
 
 
 // All routes here are prefixed by the /sales route
 module.exports = function(mongo, socket) {
-    sales = new sales(mongo, 'sales', socket);
+   sales = new sales(mongo, 'sales', socket);
 
-    router.use((req, res, next) => next()); // init
+   router.use((req, res, next) => next()); // init
 
-    /**
-     * [GET] city data with a grouping filter
-     *  query: group = brand | color_name, userID  = STRING
-     */
-    router.get('/city/:city', async (req, res) => {
-        let city = req.params.city
-        let grouping = req.query.group
-        let user = req.query.userID
-        let data = await sales.cityGroupBy(city, grouping, user);
-        res.json({
-            data: data
-        });
-    });
+   /**
+    * [GET] city data with a grouping filter
+    *  query: group = brand | color_name, userID  = STRING
+    */
+   router.get('/city/:city', sales.validation.city(), async (req, res) => {
+      sales.validation.checkResult(req, res)
 
-    /**
-     * [GET] state data with a grouping filter
-     * query: group = brand | color_name, userID = STRING
-     */
-    router.get('/state/:state', async (req, res) => {
-        let state = req.params.state
-        let grouping = req.query.group
-        let user = req.query.userID;
-        let data = await sales.stateGroupBy(state, grouping, user);
-        res.json({
-            data: data
-        });
-    });
+      let city = req.params.city
+      let grouping = req.query.group
+      let user = req.query.userID
 
-    // [GET] city state data with a grouping filter
-    // query: group = brand \ color_name, userID = STRING
-    router.get('/city/:city/state/:state', async (req, res) => {
-        let city = req.params.city;
-        let state = req.params.state;
-        let group = req.query.group;
-        let user = req.query.userID;
+      let data = await sales.cityGroupBy(city, grouping, user);
+      res.json({
+         data: data
+      });
+   });
 
-        let data = await sales.cityStateGroupBy(city, state, group, user);
-        res.json({
-            data: data
-        })
-    });
-    return router;
+   /**
+    * [GET] state data with a grouping filter
+    * query: group = brand | color_name, userID = STRING
+    */
+   router.get('/state/:state', sales.validation.state(), async (req, res) => {
+      sales.validation.checkResult(req, res);
+
+      let state = req.params.state
+      let grouping = req.query.group
+      let user = req.query.userID;
+      let data = await sales.stateGroupBy(state, grouping, user);
+      res.json({
+         data: data
+      });
+   });
+
+   // [GET] city state data with a grouping filter
+   // query: group = brand \ color_name, userID = STRING
+   router.get('/city/:city/state/:state', sales.validation.cityState(), async (req, res) => {
+      sales.validation.checkResult(req, res);
+
+      let city = req.params.city;
+      let state = req.params.state;
+      let group = req.query.group;
+      let user = req.query.userID;
+
+      let data = await sales.cityStateGroupBy(city, state, group, user);
+      res.json({
+         data: data
+      })
+   });
+   return router;
 };
