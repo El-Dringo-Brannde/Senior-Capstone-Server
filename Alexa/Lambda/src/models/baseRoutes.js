2af028837e122cfd08d9d4c4550c5b96bb1f7027
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2018-02-18 15:44:28 -0800

    Added functionality on Alexa to switch route on keyword

--- Alexa/Lambda/src/models/baseRoutes.js
+++ Alexa/Lambda/src/models/baseRoutes.js
@@ -1,54 +1,64 @@
 module.exports = class baseRoutes {
-   constructor() {
-      this.serverURL = 'http://35.169.224.183:3105/';
-      this.sessionAttributes = {};
+    constructor() {
+        this.serverURL = 'http://35.169.224.183:3105/';
+        this.sessionAttributes = {};
 
-      this.buildResponse = require('./../buildResponse');
-      this.rp = require('request-promise');
-   }
+        this.buildResponse = require('./../buildResponse');
+        this.rp = require('request-promise');
+    }
 
-   handleErr(err, callback) {
-      let speechOutput = "What was that? I couldn't understand you, please try again";
-      let repromptText = err;
-      callback(this.sessionAttributes, this.buildResponse('Error', speechOutput, repromptText, false));
-   }
+    handleErr(err, callback) {
+        let speechOutput = "What was that? I couldn't understand you, please try again";
+        let repromptText = err;
+        callback(this.sessionAttributes, this.buildResponse('Error', speechOutput, repromptText, false));
+    }
 
-   buildQueryString(intentSlots) {
-      var route = '';
-      var query = intentSlots.type.value + '/';
-      delete intentSlots.type
+    buildQueryString(intentSlots) {
+        var route = '';
+        var query = intentSlots.type.value + '/';
+        delete intentSlots.type
 
-      for (var i in intentSlots) {
-         let cur = intentSlots[i];
-         if (cur && cur.value && cur.name != 'group')
-            query += cur.name.toLowerCase() + '/' + cur.value.toLowerCase() + '/'
-         if (cur && cur.value && cur.name == 'group')
-            query += '?' + cur.name.toLowerCase() + '=' + cur.value.toLowerCase() + '&';
-      }
-      return query;
-   }
+        for (var i in intentSlots) {
+            let cur = intentSlots[i];
+            if (cur && cur.value && cur.name != 'group')
+                query += cur.name.toLowerCase() + '/' + cur.value.toLowerCase() + '/'
+            if (cur && cur.value && cur.name == 'group')
+                query += '?' + cur.name.toLowerCase() + '=' + cur.value.toLowerCase() + '&';
+        }
+        return query;
+    }
 
-   sendBackReturnedData(intentName, response, callback) {
-      response = JSON.parse(response);
-      let speechOutput = response.speechlet
-      let repromptText = "Oh noes, Something went wrong, please try again.";
-      callback(this.sessionAttributes, this.buildResponse(intentName, speechOutput, repromptText, false));
-   }
+    sendBackReturnedData(intentName, response, callback) {
+        response = JSON.parse(response);
+        let speechOutput = response.speechlet
+        let repromptText = "Oh noes, Something went wrong, please try again.";
+        callback(this.sessionAttributes, this.buildResponse(intentName, speechOutput, repromptText, false));
+    }
 
 
-   parseRoute(intent, userID, callback) {
-      const intentName = intent.name;
-      let route = this.buildQueryString(intent.slots)
-      let sessionQuery = 'userID' + '=' + userID
-      this.sendRequest(route, sessionQuery, intentName, callback)
+    parseRoute(intent, userID, callback) {
+        const intentName = intent.name;
+        this.changeView(intent.slots.view)
+        delete intent.slots.view
+        let route = this.buildQueryString(intent.slots)
+        let sessionQuery = 'userID' + '=' + userID
+        this.sendRequest(route, sessionQuery, intentName, callback)
+    }
 
-   }
+    changeView(viewType) {
+        if (viewType.value == 'map')
+            this.rp(this.serverURL + 'sales/map')
+                .then(resp => console.log(resp));
+        else
+            this.rp(this.serverURL + 'sales/home')
+                .then(resp => console.log(resp));
+    }
 
-   sendRequest(route, sessionQuery, intentName, callback) {
-      this.rp(this.serverURL + route.toLowerCase() + sessionQuery)
-         .then(resp => this.sendBackReturnedData(intentName, resp, callback))
-         .catch(err => { this.handleErr(err, callback) });
-   }
+    sendRequest(route, sessionQuery, intentName, callback) {
+        this.rp(this.serverURL + route.toLowerCase() + sessionQuery)
+            .then(resp => this.sendBackReturnedData(intentName, resp, callback))
+            .catch(err => { this.handleErr(err, callback) });
+    }
 
-   logRoute() { } // implement logic to log route here
+    logRoute() { } // implement logic to log route here
 }
--- Alexa/Lambda/src/startStop.js
+++ Alexa/Lambda/src/startStop.js
@@ -1,42 +1,30 @@
 var buildResponse = require('./buildResponse')
 
 module.exports = class startStop {
-   constructor() {
-      this.welcomeTitle = 'Welcome';
-      this.welcomeOutput = "How can I help you?";
-      this.welcomeSessionEnd = false;
-      this.exitOutput = 'Thank you for trying Look Boss, No Hands! Have a nice day!';
-      this.exitTitle = 'Session Ended';
-      this.endSessionEnd = true;
-      this.repromptText = "How can I help you?";
+    constructor() {
+        this.welcomeTitle = 'Welcome';
+        this.welcomeOutput = "How can I help you?";
+        this.welcomeSessionEnd = false;
+        this.exitOutput = 'Thank you for trying Look Boss, No Hands! Have a nice day!';
+        this.exitTitle = 'Session Ended';
+        this.endSessionEnd = true;
+        this.repromptText = "How can I help you?";
 
-      this.buildResponse = buildResponse;
+        this.buildResponse = buildResponse;
 
-      this.serverURL = 'http://35.169.224.183:3105/';
+        this.serverURL = 'http://35.169.224.183:3105/';
 
-      this.rp = require('request-promise');
-   }
+        this.rp = require('request-promise');
+    }
 
-   handleSessionEndRequest(callback) {
-      // Setting this to true ends the session and exits the skill.
-      callback({},
-         this.buildResponse(this.endTitle, this.exitOutput, null, this.endSessionEnd));
-   }
+    handleSessionEndRequest(callback) {
+        // Setting this to true ends the session and exits the skill.
+        callback({},
+            this.buildResponse(this.endTitle, this.exitOutput, null, this.endSessionEnd));
+    }
 
-   getWelcomeResponse(callback, sessionID) {
-      callback({},
-         this.buildResponse(this.welcomeTitle, this.welcomeOutput, this.repromptText, this.welcomeSessionEnd));
-      // let requestOptions = {
-      //    method: 'POST',
-      //    uri: this.serverURL + 'session/create',
-      //    body: {
-      //       sessionID: sessionID
-      //    },
-      //    json: true
-      // }
-
-      // this.rp(requestOptions)
-      //    .then(resp =>
-      //    .catch(err => this.handleErr(err, callback));
-   }
+    getWelcomeResponse(callback, sessionID) {
+        callback({},
+            this.buildResponse(this.welcomeTitle, this.welcomeOutput, this.repromptText, this.welcomeSessionEnd));
+    }
 }
