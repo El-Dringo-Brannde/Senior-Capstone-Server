514de27c1db34d789c297ab77586054db2467bb7
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2018-02-18 15:45:32 -0800

    Changed server to coincide with keyword change

--- AWS-Server/logic/sales.js
+++ AWS-Server/logic/sales.js
@@ -4,101 +4,104 @@ let salesUtility = require('./../utility/sales');
 let validation = require('./../paramValidation/sales');
 
 module.exports = class sales extends mongo {
-   constructor(mongo, collName, socket) {
-      super(mongo, collName, socket);
-      this.aggregateBuilder = new salesAgg(mongo, collName, socket);
-      this.utility = salesUtility;
-      this.validation = validation;
-   }
+    constructor(mongo, collName, socket) {
+        super(mongo, collName, socket);
+        this.aggregateBuilder = new salesAgg(mongo, collName, socket);
+        this.utility = salesUtility;
+        this.validation = validation;
+    }
 
-   async cityGroupBy(city, grouping, user) {
-      let barObj = {}, pieObj = {}, bubbleObject = {};
+    async cityGroupBy(city, grouping, user) {
+        let barObj = {}, pieObj = {}, bubbleObject = {};
 
-      let pieAgg = this.aggregateBuilder.cityPieGroupBy(city, grouping)
-      let pieRes = await this.aggregate(pieAgg)
-      pieObj = this.utility.arrayToObject(pieRes)
-      pieObj.user = user;
+        let pieAgg = this.aggregateBuilder.cityPieGroupBy(city, grouping)
+        let pieRes = await this.aggregate(pieAgg)
+        pieObj = this.utility.arrayToObject(pieRes)
+        pieObj.user = user;
 
-      let barAgg = this.aggregateBuilder.cityBarGroupBy(city, grouping)
-      let barRes = await this.aggregate(barAgg);
-      bubbleObject = JSON.parse(JSON.stringify(barRes)); // deep copy object
-      barObj = this.utility.pullGroupToObjectKey(barRes)
-      barObj.user = user;
+        let barAgg = this.aggregateBuilder.cityBarGroupBy(city, grouping)
+        let barRes = await this.aggregate(barAgg);
+        bubbleObject = JSON.parse(JSON.stringify(barRes)); // deep copy object
+        barObj = this.utility.pullGroupToObjectKey(barRes)
+        barObj.user = user;
 
-      bubbleObject = this.utility.bubbleLineGroupToObjectKey(bubbleObject);
-      bubbleObject.user = user;
+        bubbleObject = this.utility.bubbleLineGroupToObjectKey(bubbleObject);
+        bubbleObject.user = user;
 
-      this.socketIO.socket.emit('Bubble_Chart', bubbleObject);
-      this.socketIO.socket.emit('Bar_Chart', barObj);
-      this.socketIO.socket.emit('Pie_Chart', pieObj);
+        return {
+            pieChart: pieObj,
+            barChart: barObj,
+            bubbleChart: bubbleObject,
+            user: user
+        }
+        this.emitter(barObj, pieObj, bubbleObject);
+    }
 
-      return {
-         pieChart: pieObj,
-         barChart: barObj,
-         bubbleChart: bubbleObject,
-         user: user
-      }
-   }
+    async stateGroupBy(state, grouping, user) {
+        let barObj = {}, pieObj = {}, bubbleObject = {};
 
-   async stateGroupBy(state, grouping, user) {
-      let barObj = {}, pieObj = {}, bubbleObject = {};
-
-      let pieAgg = this.aggregateBuilder.statePieGroupBy(state, grouping)
-      let pieRes = await this.aggregate(pieAgg)
-      pieObj = this.utility.arrayToObject(pieRes)
-      pieObj.user = user;
+        let pieAgg = this.aggregateBuilder.statePieGroupBy(state, grouping)
+        let pieRes = await this.aggregate(pieAgg)
+        pieObj = this.utility.arrayToObject(pieRes)
+        pieObj.user = user;
 
 
-      let barAgg = this.aggregateBuilder.stateBarGroupBy(state, grouping)
-      let barRes = await this.aggregate(barAgg)
-      bubbleObject = JSON.parse(JSON.stringify(barRes)); // deep copy object
-      barObj = this.utility.pullGroupToObjectKey(barRes)
+        let barAgg = this.aggregateBuilder.stateBarGroupBy(state, grouping)
+        let barRes = await this.aggregate(barAgg)
+        bubbleObject = JSON.parse(JSON.stringify(barRes)); // deep copy object
+        barObj = this.utility.pullGroupToObjectKey(barRes)
+        barObj.user = user;
 
-      barObj.user = user;
+        bubbleObject = this.utility.bubbleLineGroupToObjectKey(bubbleObject);
+        bubbleObject.user = user;
 
+        return {
+            pieChart: pieObj,
+            barChart: barObj,
+            bubbleChart: bubbleObject,
+            user: user
+        }
+        this.emitter(barObj, pieObj, bubbleObject);
+    }
 
-      bubbleObject = this.utility.bubbleLineGroupToObjectKey(bubbleObject);
-      bubbleObject.user = user;
+    async cityStateGroupBy(city, state, group, user) {
+        let pieObject = {}, barObject = {}, bubbleObject = {};
 
-      this.socketIO.socket.emit('Bar_Chart', barObj);
-      this.socketIO.socket.emit('Pie_Chart', pieObj);
-      this.socketIO.socket.emit('Bubble_Chart', bubbleObject);
+        let pieAgg = this.aggregateBuilder.cityStatePieGroupBy(city, state, group);
+        pieObject = await this.aggregate(pieAgg)
+        pieObject = this.utility.arrayToObject(pieObject)
+        pieObject.user = user;
 
-      return {
-         pieChart: pieObj,
-         barChart: barObj,
-         bubbleChart: bubbleObject,
-         user: user
-      }
-   }
+        let barAgg = this.aggregateBuilder.cityStateBarGroupBy(city, state, group)
+        barObject = await this.aggregate(barAgg);
+        bubbleObject = JSON.parse(JSON.stringify(barObject)); // deep copy object
 
-   async cityStateGroupBy(city, state, group, user) {
-      let pieObject = {}, barObject = {}, bubbleObject = {};
+        barObject = this.utility.pullGroupToObjectKey(barObject);
+        barObject.user = user;
 
-      let pieAgg = this.aggregateBuilder.cityStatePieGroupBy(city, state, group);
-      pieObject = await this.aggregate(pieAgg)
-      pieObject = this.utility.arrayToObject(pieObject)
-      pieObject.user = user;
+        bubbleObject = this.utility.bubbleLineGroupToObjectKey(bubbleObject);
+        bubbleObject.user = user;
 
-      let barAgg = this.aggregateBuilder.cityStateBarGroupBy(city, state, group)
-      barObject = await this.aggregate(barAgg);
-      bubbleObject = JSON.parse(JSON.stringify(barObject)); // deep copy object
+        return {
+            pieChart: pieObject,
+            barChart: barObject,
+            bubbleChart: bubbleObject,
+            user: user
+        }
+        this.emitter(barObject, pieObject, bubbleObject);
+    } // ghetto way of doing math NOT in mongoDB
 
-      barObject = this.utility.pullGroupToObjectKey(barObject);
-      barObject.user = user;
+    changeViewToHome() {
+        this.socketIO.socket.emit('home');
+    }
 
-      bubbleObject = this.utility.bubbleLineGroupToObjectKey(bubbleObject);
-      bubbleObject.user = user;
+    changeViewToMap() {
+        this.socketIO.socket.emit('map');
+    }
 
-      this.socketIO.socket.emit('Bar_Chart', barObject);
-      this.socketIO.socket.emit('Pie_Chart', pieObject);
-      this.socketIO.socket.emit('Bubble_Chart', bubbleObject);
-
-      return {
-         pieChart: pieObject,
-         barChart: barObject,
-         bubbleChart: bubbleObject,
-         user: user
-      } // ghetto way of doing math NOT in mongoDB
-   }
+    emitter(barObj, pieObj, bubbleObj) {
+        this.socketIO.socket.emit('Bar_Chart', barObj);
+        this.socketIO.socket.emit('Pie_Chart', pieObj);
+        this.socketIO.socket.emit('Bubble_Chart', bubbleObj);
+    }
 }
--- AWS-Server/routes/sales.js
+++ AWS-Server/routes/sales.js
@@ -8,101 +8,115 @@ let states = require('./../logic/state');
 
 // All routes here are prefixed by the /sales route
 module.exports = function(mongo, socket) {
-   sales = new sales(mongo, 'sales', socket);
-   logger = new logger(mongo, 'queries', null);
-   states = new states(mongo, 'states', null);
-   speechlet = new speechlet();
+    sales = new sales(mongo, 'sales', socket);
+    logger = new logger(mongo, 'queries', null);
+    states = new states(mongo, 'states', null);
+    speechlet = new speechlet();
 
-   router.use((req, res, next) => next()); // init
-   /**
-    * [GET] city data with a grouping filter
-    *  query: group = brand | color_name, userID  = STRING
-    */
-   router.get('/city/:city', sales.validation.city(), async (req, res) => {
-      sales.validation.checkResult(req, res)
+    router.use((req, res, next) => next()); // init
+    /**
+     * [GET] city data with a grouping filter
+     *  query: group = brand | color_name, userID  = STRING
+     */
+    router.get('/city/:city', sales.validation.city(), async (req, res) => {
+        sales.validation.checkResult(req, res)
 
-      let city = req.params.city
-      let grouping = req.query.group
-      let user = req.query.userID
+        let city = req.params.city
+        let grouping = req.query.group
+        let user = req.query.userID
 
-      let data = await sales.cityGroupBy(city, grouping, user);
+        let data = await sales.cityGroupBy(city, grouping, user);
 
-      let speechResponse = speechlet.repeatSpeechlet(city, '', grouping, data);
+        let speechResponse = speechlet.repeatSpeechlet(city, '', grouping, data);
 
-      res.json({
-         data: data,
-         speechlet: speechResponse
-      });
-      logAndUpdate(req, user)
-   });
+        res.json({
+            data: data,
+            speechlet: speechResponse
+        });
+        logAndUpdate(req, user)
+    });
 
-   /**
-    * [GET] state data with a grouping filter
-    * query: group = brand | color_name, userID = STRING
-    */
-   router.get('/state/:state', sales.validation.state(), async (req, res) => {
-      sales.validation.checkResult(req, res);
+    /**
+     * [GET] state data with a grouping filter
+     * query: group = brand | color_name, userID = STRING
+     */
+    router.get('/state/:state', sales.validation.state(), async (req, res) => {
+        sales.validation.checkResult(req, res);
 
-      let state = req.params.state
-      let grouping = req.query.group
-      let user = req.query.userID;
-      let data = await sales.stateGroupBy(state, grouping, user);
-      let speechResponse = speechlet.repeatSpeechlet('', state, grouping, data);
+        let state = req.params.state
+        let grouping = req.query.group
+        let user = req.query.userID;
+        let data = await sales.stateGroupBy(state, grouping, user);
+        let speechResponse = speechlet.repeatSpeechlet('', state, grouping, data);
 
-      res.json({
-         data: data,
-         speechlet: speechResponse
-      });
-      logAndUpdate(req, user)
-   });
+        res.json({
+            data: data,
+            speechlet: speechResponse
+        });
+        logAndUpdate(req, user)
+    });
 
-   // [GET] city state data with a grouping filter
-   // query: group = brand \ color_name, userID = STRING
-   router.get('/city/:city/state/:state', sales.validation.cityState(), async (req, res) => {
-      sales.validation.checkResult(req, res);
+    // [GET] city state data with a grouping filter
+    // query: group = brand \ color_name, userID = STRING
+    router.get('/city/:city/state/:state', sales.validation.cityState(), async (req, res) => {
+        sales.validation.checkResult(req, res);
 
-      let city = req.params.city;
-      let state = req.params.state;
-      let grouping = req.query.group;
-      let user = req.query.userID;
+        let city = req.params.city;
+        let state = req.params.state;
+        let grouping = req.query.group;
+        let user = req.query.userID;
 
-      let data = await sales.cityStateGroupBy(city, state, grouping, user);
-      let speechResponse = speechlet.repeatSpeechlet(city, state, grouping, data);
-
-
-      res.json({
-         data: data,
-         speechlet: speechResponse
-      })
-      logAndUpdate(req, user)
-   });
-
-   // [GET] city state data with a grouping filter
-   // query: group = brand \ color_name, userID = STRING
-   router.get('/state/:state/city/:city', sales.validation.cityState(), async (req, res) => {
-      sales.validation.checkResult(req, res);
-
-      let city = req.params.city;
-      let state = req.params.state;
-      let grouping = req.query.group;
-      let user = req.query.userID;
-
-      let data = await sales.cityStateGroupBy(city, state, grouping, user);
-      let speechResponse = speechlet.repeatSpeechlet(city, state, grouping, data);
+        let data = await sales.cityStateGroupBy(city, state, grouping, user);
+        let speechResponse = speechlet.repeatSpeechlet(city, state, grouping, data);
 
 
-      res.json({
-         data: data,
-         speechlet: speechResponse
-      })
-      logAndUpdate(req, user)
-   });
+        res.json({
+            data: data,
+            speechlet: speechResponse
+        })
+        logAndUpdate(req, user)
+    });
+
+    // [GET] city state data with a grouping filter
+    // query: group = brand \ color_name, userID = STRING
+    router.get('/state/:state/city/:city', sales.validation.cityState(), async (req, res) => {
+        sales.validation.checkResult(req, res);
+
+        let city = req.params.city;
+        let state = req.params.state;
+        let grouping = req.query.group;
+        let user = req.query.userID;
+
+        let data = await sales.cityStateGroupBy(city, state, grouping, user);
+        let speechResponse = speechlet.repeatSpeechlet(city, state, grouping, data);
 
 
-   function logAndUpdate(req, user) {
-      logger.logRoute(req, user);
-      states.updateState(req, user);
-   }
-   return router;
+        res.json({
+            data: data,
+            speechlet: speechResponse
+        })
+        logAndUpdate(req, user)
+    });
+
+    // used to change the view in the VR environment
+    router.get('/home', async (req, res) => {
+        sales.changeViewToHome();
+        res.json({
+            data: 'ok'
+        });
+    });
+
+    router.get('/map', async (req, res) => {
+        sales.changeViewToMap();
+        res.json({
+            data: 'ok'
+        });
+    });
+
+    function logAndUpdate(req, user) {
+        logger.logRoute(req, user);
+        states.updateState(req, user);
+    }
+    return router;
 };
 
