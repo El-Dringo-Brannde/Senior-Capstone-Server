fe13b18618c822199ce8ccbaec0c4a091376428e
Unknown <bensocar@oregonstate.edu>
2018-05-10 21:16:26 -0700

    Fixed variable scope issue, finished creating all functions, put result print in try/catch for view change requests which return nothing

--- Testing/makeRequest.py
+++ Testing/makeRequest.py
@@ -1,3 +1,6 @@
+#A python script to hopefully make testing/grading the project easier
+#Simulate the requests Alexa makes for user requests
+
 import json
 try:
     import urllib.request
@@ -5,11 +8,16 @@ except ImportError:
     print('Error importing urllib. This script needs to be run using Python3')
     quit()
 
-city = ''
 state = ''
+city = ''
 grouping = ''
+view = 'home'
 
+#Build the query string for a new request
 def buildRequest():
+    global city
+    global state
+    global grouping
     serverURL = 'http://35.169.224.183:3105/sales/'
     userID = 'amzn1.ask.account.testing'
 
@@ -17,23 +25,36 @@ def buildRequest():
     if(city != ''):
         requestURL = requestURL + 'city/' + city + '/'
 
-    if(city != ''):
+    if(state != ''):
         requestURL = requestURL + 'state/' + state +'/'
 
     requestURL = requestURL + '?group=' + grouping + '&userID=' + userID
-    print('Requesting ' + requestURL)
+
     return requestURL
 
-def makeRequest(requestURL):
-    return urllib.request.urlopen(requestURL)
+#Accept a suggestion and make the request
+def accept_suggestion():
+    #Get the last suggestion for the user
+    serverURL = 'http://35.169.224.183:3105/sales/last/user/'
+    userID = 'amzn1.ask.account.testing'
 
-def parseResult(reqResult):
+    req = serverURL + userID
+    makeRequest(req)
+
+#Send the request and return what Alexa would have said
+def makeRequest(requestURL):
+    reqResult = urllib.request.urlopen(requestURL)
+
     data = reqResult.read()
     dataEncoding = reqResult.info().get_content_charset('utf-8')
     jsonData = json.loads(data.decode(dataEncoding))
 
-    print(jsonData['speechlet'])
+    try:
+        print(jsonData['speechlet'])
+    except KeyError:
+        print('')
 
+#Print out a list of available commands
 def printHelp():
     print('Commands:')
     print('help - Prints this list')
@@ -44,7 +65,10 @@ def printHelp():
     print('list - Show the currently defined request')
     print('run - Make the request')
     print('clear - Clear the currently defined request')
+    print('view - Switch view between house and map')
+    print('yes - Only works after a request, used to follow the suggested request')
 
+#Intro to be shown upon startup
 def printIntro():
     print('****************************')
     print('This script allows for testing the VR portion of the project without needing to set up an Echo with the custom commands')
@@ -53,35 +77,62 @@ def printIntro():
     printHelp()
     print('****************************')
 
+#Get a state to request
 def setState():
+    global state
     print('Enter a new state for the request or blank to clear the current value of ' + state)
     newState = input('> ')
 
     state = newState
 
+#Get a city to request
 def setCity():
+    global city
     print('Enter a new city for the request or blank to clear the current value of ' + city)
     newCity = input('> ')
 
     city = newCity
 
+#Change the grouping method between color and brand
 def setGrouping():
+    global grouping
     print('Enter either "brand" or "color" to set what to group sales by. Currently ' + grouping)
     newGrouping = input('> ')
 
-    if(newGrouping == 'brand' || newGrouping == 'color'):
+    if(newGrouping == 'brand' or newGrouping == 'color'):
         grouping = newGrouping
     else:
         print('Invalid entry, please try again')
         setGrouping()
 
+#Swap the VR view between the house and map overview
+def swap_view():
+    global city
+    global state
+    global view
+    global grouping
+    serverURL = 'http://35.169.224.183:3105/sales/'
+    userID = 'amzn1.ask.account.testing'
 
+    if(view == 'home'):
+        query = '/map/name/none/state/' + state + '/city/' + city + '/?userID=' + userID + '&group=' + grouping
+        req = makeRequest(serverURL + query)
+        view = 'map'
+    else:
+        req = makeRequest(serverURL + 'home/')
+        view = 'home'
+
+#Get user input for a command to run
 def getCommand():
+    global city
+    global state
+    global grouping
+
     command = input('Command: ')
     if(command == 'help'):
         printIntro()
     elif(command == 'demos'):
-
+        print('demo stuff')
     elif(command == 'city'):
         setCity()
     elif(command == 'state'):
@@ -90,12 +141,15 @@ def getCommand():
         setGrouping()
     elif(command == 'list'):
         print('City: ' + city + '; State: '+ state + '; Group by: ' + grouping)
+    elif(command == 'view'):
+        swap_view()
+    elif(command == 'yes'):
+        accept_suggestion()
     elif(command == 'run'):
-        if(grouping != '' && (city != '' || state != '')):
-            print('You could make this request to Alexa by saying "Show me sales by ' + grouping + ' in ' + city + ', ' + state + '."')
+        if(grouping != '' and (city != '' or state != '')):
+            print('You could make this request to Alexa by saying "Show me sales by ' + grouping + ' in ' + city + ', ' + state + '"')
             req = buildRequest()
-            res = makeRequest(req)
-            parseResult(res)
+            makeRequest(req)
         else:
             print('Please set some values to request. A request needs at least a city or state as well as grouping type')
 
@@ -109,4 +163,5 @@ def getCommand():
     getCommand()
 
 printIntro()
+
 getCommand()
